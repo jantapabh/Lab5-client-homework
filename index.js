f005f6492b70704542cccfc7ef2fbd2f8929af26
jantapabh <jantapa2407@gmail.com>
2020-02-26 14:53:12 +0700

    test commit

--- index.js
+++ index.js
@@ -12,20 +12,20 @@ app.get('/computer', function (req, res) {
         {
             computers:
 
-                [{
+                [{  topic: '1',
                     name: 'Windows',
                     images: 'https://www.howtogeek.com/wp-content/uploads/2020/01/win10-on-7-banner.png'
                 },
-                {
-                    name: 'OSX',
+                {   topic: '2',
+                    name: 'OSX    ',
                     images: 'https://support.apple.com/library/content/dam/edam/applecare/images/en_US/osx/elcapitan-update-software-app-store-hero-alt.jpg'
                 },
-                {
-                    name: 'Andriod',
-                    images: 'https://www.lcdtvthailand.com/images/topic/Android_Play_Store_Malwear.jpg'
+                {   topic: '3',
+                    name: 'Andriod ',
+                    images: 'https://www.flashfly.net/wp/wp-content/uploads/2019/01/android.jpg'
                 },
-                {
-                    name: 'IOS',
+                {   topic: '4',
+                    name: 'IOS     ',
                     images: 'https://sites.google.com/site/jukkridrenji12/_/rsrc/1447515442900/rabb-ptibati-kar-ios/i6.png'
                 }
 
