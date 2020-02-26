e508ff03e2720512e8ef417779ecc7f582b10f50
jantapabh <jantapa2407@gmail.com>
2020-02-26 16:31:45 +0700

    commit session

--- app.js
+++ app.js
@@ -1,31 +1,80 @@
 var express = require('express')
 var app = express()
 bodyParser = require('body-parser');
-var session = require('express-session')
+var session = require('express-session');
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-app.use(express.static(__dirname + '/public'));
 
-app.post('/admin', urlencodedParser, function (req, res) {
-    var result = parseInt(req.body.a) + parseInt(req.body.b);
-    res.send('Result = ' + result);
+app.use(session({
+
+    secret: 'keyboard cat',
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
+}))
+
+
+app.use(function (req, res, next) {
+
+    var sess = req.session
+
+    if (sess.views) {
+
+        sess.views++
+
+    } else {
+
+        sess.views = 1
+    }
+
+    next();
+})
+
+app.get('/logout', (req, res) => {
+
+    req.session.destroy((err) => {
+
+        if (err) {
+            console.log(err);
+
+        } else {
+
+            res.redirect('/');
+        }
+    });
 });
 
 
 app.get('/', function (req, res) {
-    res.render('user', {
 
-        user:
-            [{
-                email: ' ',
-                password: '240311'
-            }], foo: 'Login'
-    })
+    let password = "240311";
+
+    res.send(`<html><body>
+
+    <form>
+     Email : <input type="email" placeholder="Type your email" />
+     Password: <input type="number" placeholder="Type your password" />
+    </form>
+    <button type="submit">submit</button>
+    </body></html>`)
 })
+
+app.get('/logout', (req, res) => {
+
+    req.session.destroy((err) => {
+
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
+
+    });
+});
+
+
 app.listen(8000);
 
 
-app.listen(8000)
